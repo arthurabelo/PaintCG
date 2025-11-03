67ac5018d41f64f723359bb5c5caf21744c89416
ARTHUR RABELO DE CARVALHO <rabeloarthur@ufpi.edu.br>
2025-11-02 22:33:14 -0300

    Rearrange UI text positions and update shortcut display
    Moved the mouse coordinates display higher, adjusted the mode text position, and updated the shortcut instructions for better clarity and alignment in the UI.

--- main.cpp
+++ main.cpp
@@ -845,14 +845,13 @@ void redrawAll()
 
     // Redesenha texto de coordenadas e instruções
     glColor3f(0, 0, 0);
-    draw_text_stroke(sidebarWidth + 5, 5, string("Modo: ") + (modo == M_LINHA ? "Linha" : modo == M_RETANGULO ? "Retangulo"
+    draw_text_stroke(sidebarWidth + 5, 35, string("(") + to_string(mouse_x) + string(", ") + to_string(mouse_y) + string(")"), 0.09);
+    draw_text_stroke(sidebarWidth + 5, 22, string("Modo: ") + (modo == M_LINHA ? "Linha" : modo == M_RETANGULO ? "Retangulo"
                                                                        : modo == M_TRIANGULO   ? "Triangulo"
                                                                        : modo == M_POLIGONO    ? "Poligono"
                                                                                                : "Circulo"),
                      0.15);
-    // Mostrar coordenadas do mouse
-    draw_text_stroke(sidebarWidth + 5, 20, string("(") + to_string(mouse_x) + string(", ") + to_string(mouse_y) + string(")"), 0.09);
-    draw_text_stroke(sidebarWidth + 5, 35, string("Atalhos: l=linha r=ret t=tri p=pol c=circ f=scanfill o=flood x=clear esc=sair"), 0.08);
+    draw_text_stroke(sidebarWidth + 5, 5, string("Atalhos: l = linha r = ret t = tri p = pol c = circ f = scanfill o = flood x = clear esc = sair"), 0.09);
 
     glutSwapBuffers();
 }
